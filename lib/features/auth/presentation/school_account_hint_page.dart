33a3763b3a6dde5533cca041c750e0c749e99e2d
SiongSng <rrt467778@gmail.com>
2025-01-22 23:51:48 +0800

    ci: deploy web

--- lib/features/auth/presentation/school_account_hint_page.dart
+++ lib/features/auth/presentation/school_account_hint_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' show Divider, Scaffold;
 import 'package:flutter/widgets.dart';
